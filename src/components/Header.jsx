7ed52593a111557402b10222629da74f7c2ee99c
jim walter <jimwalter13090@gmail.com>
2020-08-13 14:52:45 -0600

    added personal favicon

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,8 +4,8 @@ import "./Header.css";
 function Header() {
   return (
     <header id="home" className="header">
-      <h1 className="logo">Jim Walter</h1>
-      <p className="sub">Software Engineer, Maker, Problem Solver</p>
+      <h1 className="logo">JIM WALTER</h1>
+      <p className="sub">Software Engineer // Maker // Problem Solver</p>
     </header>
   );
 }
