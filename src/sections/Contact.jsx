4d38bd8479aab919256857627410d06f254f45d3
jim walter <jimwalter13090@gmail.com>
2020-08-13 16:28:24 -0600

    added social logos and links to Contact section

--- src/sections/Contact.jsx
+++ src/sections/Contact.jsx
@@ -1,24 +1,35 @@
 import React from "react";
 import "./Contact.css";
+import emailLogo from "../assets/images/gmail_icon.png";
+import LinkedInLogo from "../assets/images/linked_icon.png";
+import githubLogo from "../assets/images/github_icon.png";
 
 function Contact() {
   return (
-    <div id="contact" className="section">
+    <div className="contact section">
       <h2 className="headline">Contact</h2>
-      <div className="contact-info">
-        <img src="" alt="email logo" />
-        <h4>email</h4>
-        <p>jimwalter13090@gmail.com</p>
+      <div>
+        <a href="mailto: jimwalter13090@gmail.com" target="_blank" rel="noopener noreferrer" className="contact-info">
+          <img src={emailLogo} alt="email icon"></img>
+          <p>Send Email</p>
+        </a>
       </div>
-      <div className="contact-info">
-        <img src="../assets/images/socials/linked_icon.png" alt="LinkedIn logo" />
-        <h4>LinkedIn</h4>
-        <a href="https://www.linkedin.com/in/jimwalter01/">https://www.linkedin.com/in/jimwalter01/</a>
+      <div>
+        <a
+          href="https://www.linkedin.com/in/jimwalter01/"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="contact-info"
+        >
+          <img src={LinkedInLogo} alt="LinkedIn logo" />
+          <p>LinkedIn</p>
+        </a>
       </div>
-      <div className="contact-info">
-        <img src="../assets/images/socials/linked_icon.png" alt="GitHub logo" />
-        <h4>GitHub</h4>
-        <a href="https://github.com/jimwalter">https://github.com/jimwalter</a>
+      <div>
+        <a href="https://github.com/jimwalter" target="_blank" rel="noopener noreferrer" className="contact-info">
+          <img src={githubLogo} alt="LinkedIn logo"></img>
+          <p>GitHub</p>
+        </a>
       </div>
     </div>
   );
