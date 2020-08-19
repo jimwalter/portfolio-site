070f3c2f89c9e8e48c93180948d70a08c1142def
jim walter <jimwalter13090@gmail.com>
2020-08-19 12:54:16 -0600

    added video background to header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,14 @@
 import React from "react";
 import "./Header.css";
+import grass from "../assets/videos/trees.mp4";
 
 function Header() {
   return (
     <header id="home" className="header">
+      <div className="fullscreen-video-wrap">
+        <video src={grass} loop autoPlay muted preload="true"></video>
+      </div>
+      <div className="header-overlay"></div>
       <div className="header-content">
         <h1 className="logo">JIM WALTER</h1>
         <p className="sub">Software Engineer // Maker // Problem Solver</p>
