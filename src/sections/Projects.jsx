85e2c69d24cf4cc061d9aa3341d4a4e31d8416af
jim walter <jimwalter13090@gmail.com>
2020-08-20 16:03:39 -0600

    mapping from project JSON file

--- src/sections/Projects.jsx
+++ src/sections/Projects.jsx
@@ -20,11 +20,9 @@ function Projects() {
           <p className="description">{proj[0].p1}</p>
           <div className="features">
             <h5>Key Features</h5>
-            <p>&#8212; Page Routing using React Router</p>
-            <p>&#8212; UI/UX animations using GSAP library</p>
-            <p>&#8212; Deployed on AWS S3</p>
-            <p>&#8212; Responsive Design</p>
-            <p>&#8212; Cross Browser Compatibility</p>
+            {proj[0].points.map((point) => (
+              <p>&#8212; {point} </p>
+            ))}
           </div>
         </div>
         <a className="project-img" href={proj[0].address} target="_blank" rel="noopener noreferrer">
@@ -39,13 +37,9 @@ function Projects() {
           <p className="description">{proj[1].p1}</p>
           <div className="features">
             <h5 className="features">Key Features</h5>
-            <p>&#8212; Populated MongoDB &amp; PostgreSQL databases with 10M vehicle records</p>
-            <p>&#8212; Created relavent APIs using Node/Express for inherited front end</p>
-            <p>&#8212; Tuned DB performance with indexing &amp; Redis caching</p>
-            <p className="psub">(400% improvement for Postgres, 150% improvement for MongoDB)</p>
-            <p>&#8212; Stress tested using Artillery.io</p>
-            <p>&#8212; Achieved 1,000 RPS on locally</p>
-            <p>&#8212; Deployed with Docker &amp; AWS</p>
+            {proj[1].points.map((point) => (
+              <p>&#8212; {point} </p>
+            ))}
           </div>
         </div>
         <a className="project-img" href={proj[1].address} target="_blank" rel="noopener noreferrer">
@@ -60,11 +54,9 @@ function Projects() {
           <p className="description">{proj[2].p1}</p>
           <div className="features">
             <h5 className="features">Key Features</h5>
-            <p>&#8212; Data integration with Yelp Fusion API</p>
-            <p>&#8212; </p>
-            <p>&#8212; </p>
-            <p>&#8212; </p>
-            <p>&#8212; </p>
+            {proj[2].points.map((point) => (
+              <p>&#8212; {point} </p>
+            ))}
           </div>
         </div>
         <a className="project-img" href={proj[2].address} target="_blank" rel="noopener noreferrer">
