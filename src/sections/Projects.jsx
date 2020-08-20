5ac96455a053a506d2a347ab5e706069968a024e
jim walter <jimwalter13090@gmail.com>
2020-08-20 15:30:08 -0600

    removed lists from projects, proj stays centered wider

--- src/sections/Projects.jsx
+++ src/sections/Projects.jsx
@@ -18,14 +18,14 @@ function Projects() {
             <h3 className="title">{proj[0].title}</h3>
           </a>
           <p className="description">{proj[0].p1}</p>
-          <h5 className="features">Key Features</h5>
-          <ul>
-            <li>Page Routing using React Router</li>
-            <li>UI/UX animations using GSAP library</li>
-            <li>Deployed on AWS S3</li>
-            <li>Responsive Design</li>
-            <li>Cross Browser Compatibility</li>
-          </ul>
+          <div className="features">
+            <h5>Key Features</h5>
+            <p>&#8212; Page Routing using React Router</p>
+            <p>&#8212; UI/UX animations using GSAP pbrary</p>
+            <p>&#8212; Deployed on AWS S3</p>
+            <p>&#8212; Responsive Design</p>
+            <p>&#8212; Cross Browser Compatibipty</p>
+          </div>
         </div>
         <a className="project-img" href={proj[0].address} target="_blank" rel="noopener noreferrer">
           <img src={giide_1} alt="screenshot of Giide.fm website" />
@@ -37,22 +37,17 @@ function Projects() {
             <h3 className="title">{proj[1].title}</h3>
           </a>
           <p className="description">{proj[1].p1}</p>
-          <h5 className="features">Key Features</h5>
-          <ul>
-            <li>Populated MongoDB and PostgreSQL databases with 10M vehicle records</li>
-            <li>Created and connected relavent APIs using Node.js/Express for inherited front end</li>
-            <li>
-              Tuned DB performance with indexing and Redis caching
-              <ul>
-                <li>400% improvement for Postgres</li>
-                <li>150% improvement for MongoDB</li>
-              </ul>
-            </li>
-
-            <li>Stress tested database using Artillery.io</li>
-            <li>Achieved 1,000 RPS on local Postgres instance</li>
-            <li>Containerized and deployed with Docker and AWS</li>
-          </ul>
+          <div className="features">
+            <h5 className="features">Key Features</h5>
+            <p>&#8212; Populated MongoDB &amp; PostgreSQL databases with 10M vehicle records</p>
+            <p>&#8212; Created relavent APIs using Node/Express for inherited front end</p>
+            <p>&#8212; Tuned DB performance with indexing &amp; Redis caching</p>
+            <p>&#8212; 400% improvement for Postgres</p>
+            <p>&#8212; 150% improvement for MongoDB</p>
+            <p>&#8212; Stress tested using Artillery.io</p>
+            <p>&#8212; Achieved 1,000 RPS on locally</p>
+            <p>&#8212; Deployed with Docker &amp; AWS</p>
+          </div>
         </div>
         <a className="project-img" href={proj[1].address} target="_blank" rel="noopener noreferrer">
           <img src={sdc_gif} alt="img2" />
@@ -64,14 +59,14 @@ function Projects() {
             <h3 className="title">{proj[2].title}</h3>
           </a>
           <p className="description">{proj[2].p1}</p>
-          <h5 className="features">Key Features</h5>
-          <ul>
-            <li>Data integration with Yelp Fusion API</li>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
-          </ul>
+          <div className="features">
+            <h5 className="features">Key Features</h5>
+            <p>&#8212; Data integration with Yelp Fusion API</p>
+            <p>&#8212; </p>
+            <p>&#8212; </p>
+            <p>&#8212; </p>
+            <p>&#8212; </p>
+          </div>
         </div>
         <a className="project-img" href={proj[2].address} target="_blank" rel="noopener noreferrer">
           <img src={nea_1} alt="img3" />
