1a4e8419367528641d286d2abb2f4753f689889e
jim walter <jimwalter13090@gmail.com>
2020-08-20 15:08:56 -0600

    added skill icons, project content

--- src/sections/Projects.jsx
+++ src/sections/Projects.jsx
@@ -1,67 +1,79 @@
 import React from "react";
+
 import "./Projects.css";
+import projects from "../json_projects.json";
 
 import giide_1 from "../assets/screenshots/giide_1.png";
 import sdc_gif from "../assets/screenshots/sdc_1.gif";
 import nea_1 from "../assets/screenshots/NEA_1.png";
-// import nea_2 from "../assets/images/NEA_2.png";
-// import nea_3 from "../assets/images/NEA_3.png";
 
 function Projects() {
+  var proj = projects.data;
   return (
     <div className="projects section">
       <h2 className="headline">Projects</h2>
       <div className="project left">
         <div className="leader">
-          <a href="https://www.giide.fm/" target="_blank" rel="noopener noreferrer">
-            <h3 className="title">Giide.fm Website</h3>
+          <a href={proj[0].address} target="_blank" rel="noopener noreferrer">
+            <h3 className="title">{proj[0].title}</h3>
           </a>
-          <p className="description">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Error commodi corrupti magnam unde veniam
-            cupiditate eos, ducimus, laborum atque molestias possimus sapiente repudiandae hic recusandae accusantium,
-            rerum praesentium ex non!
-          </p>
+          <p className="description">{proj[0].p1}</p>
+          <h5 className="features">Key Features</h5>
+          <ul>
+            <li>Page Routing using React Router</li>
+            <li>UI/UX animations using GSAP library</li>
+            <li>Deployed on AWS S3</li>
+            <li>Responsive Design</li>
+            <li>Cross Browser Compatibility</li>
+          </ul>
         </div>
-        <a className="project-img" href="https://www.giide.fm/" target="_blank" rel="noopener noreferrer">
-          <img src={giide_1} alt="img1" />
+        <a className="project-img" href={proj[0].address} target="_blank" rel="noopener noreferrer">
+          <img src={giide_1} alt="screenshot of Giide.fm website" />
         </a>
       </div>
       <div className="project right">
         <div className="leader">
-          <a href="https://github.com/SDC-ghrden03/similar-vehicles" target="_blank" rel="noopener noreferrer">
-            <h3 className="title">Used Car Similar Vehicle Service</h3>
+          <a href={proj[1].address} target="_blank" rel="noopener noreferrer">
+            <h3 className="title">{proj[1].title}</h3>
           </a>
-          <p className="description">
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Hic cum, vero, atque enim ex incidunt recusandae
-            nihil et, at nesciunt nostrum corporis aut dolores dolorem quod minus blanditiis eum quam?
-          </p>
+          <p className="description">{proj[1].p1}</p>
+          <h5 className="features">Key Features</h5>
+          <ul>
+            <li>Populated MongoDB and PostgreSQL databases with 10M vehicle records</li>
+            <li>Created and connected relavent APIs using Node.js/Express for inherited front end</li>
+            <li>
+              Tuned DB performance with indexing and Redis caching
+              <ul>
+                <li>400% improvement for Postgres</li>
+                <li>150% improvement for MongoDB</li>
+              </ul>
+            </li>
+
+            <li>Stress tested database using Artillery.io</li>
+            <li>Achieved 1,000 RPS on local Postgres instance</li>
+            <li>Containerized and deployed with Docker and AWS</li>
+          </ul>
         </div>
-        <a
-          className="project-img"
-          href="https://github.com/SDC-ghrden03/similar-vehicles"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <a className="project-img" href={proj[1].address} target="_blank" rel="noopener noreferrer">
           <img src={sdc_gif} alt="img2" />
         </a>
       </div>
       <div className="project left ">
         <div className="leader">
-          <a href="https://neighborly-eats.herokuapp.com/" target="_blank" rel="noopener noreferrer">
-            <h3 className="title">Neighborly Eats App</h3>
+          <a href={proj[2].address} target="_blank" rel="noopener noreferrer">
+            <h3 className="title">{proj[2].title}</h3>
           </a>
-          <p className="description">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit obcaecati vitae inventore, cumque officiis
-            placeat ad alias assumenda repudiandae ea omnis delectus aspernatur impedit maiores, voluptatibus non
-            necessitatibus mollitia laboriosam?
-          </p>
+          <p className="description">{proj[2].p1}</p>
+          <h5 className="features">Key Features</h5>
+          <ul>
+            <li>Data integration with Yelp Fusion API</li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+          </ul>
         </div>
-        <a
-          className="project-img"
-          href="https://neighborly-eats.herokuapp.com/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <a className="project-img" href={proj[2].address} target="_blank" rel="noopener noreferrer">
           <img src={nea_1} alt="img3" />
         </a>
       </div>
