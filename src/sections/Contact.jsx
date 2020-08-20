834bbdfce4ad1358a4da31584ac35057f3c7df78
jim walter <jimwalter13090@gmail.com>
2020-08-20 17:43:12 -0600

    reformatted contact section with Flexbox

--- src/sections/Contact.jsx
+++ src/sections/Contact.jsx
@@ -8,28 +8,31 @@ function Contact() {
   return (
     <div className="contact section">
       <h2 className="headline">Contact</h2>
-      <div className="contact-item">
-        <a href="mailto: jimwalter13090@gmail.com" target="_blank" rel="noopener noreferrer" className="contact-info">
-          <img src={emailLogo} alt="email icon"></img>
-          <p>Send Email</p>
-        </a>
-      </div>
-      <div className="contact-item">
-        <a
-          href="https://www.linkedin.com/in/jimwalter01/"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="contact-info"
-        >
-          <img src={LinkedInLogo} alt="LinkedIn logo" />
-          <p>LinkedIn</p>
-        </a>
-      </div>
-      <div className="contact-item">
-        <a href="https://github.com/jimwalter" target="_blank" rel="noopener noreferrer" className="contact-info">
-          <img src={githubLogo} alt="LinkedIn logo"></img>
-          <p>GitHub</p>
-        </a>
+      <div className="contacts">
+        <div className="contact-item">
+          <a href="mailto: jimwalter13090@gmail.com" target="_blank" rel="noopener noreferrer">
+            <div className="contact-link">
+              <img src={emailLogo} alt="email icon"></img>
+              <p>Send Email</p>
+            </div>
+          </a>
+        </div>
+        <div className="contact-item">
+          <a href="https://www.linkedin.com/in/jimwalter01/" target="_blank" rel="noopener noreferrer">
+            <div className="contact-link">
+              <img src={LinkedInLogo} alt="LinkedIn logo" />
+              <p>LinkedIn</p>
+            </div>
+          </a>
+        </div>
+        <div className="contact-item">
+          <a href="https://github.com/jimwalter" target="_blank" rel="noopener noreferrer">
+            <div className="contact-link">
+              <img src={githubLogo} alt="LinkedIn logo"></img>
+              <p>GitHub</p>
+            </div>
+          </a>
+        </div>
       </div>
     </div>
   );
