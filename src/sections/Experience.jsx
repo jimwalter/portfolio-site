12e9037396ae253595ac639c17445b88264daac1
jim walter <jimwalter13090@gmail.com>
2020-08-20 18:36:54 -0600

    added mapping from Exp JSON file and resizing styles

--- src/sections/Experience.jsx
+++ src/sections/Experience.jsx
@@ -1,17 +1,19 @@
 import React from "react";
 import "./Experience.css";
+import data from "../json_experience.json";
+
 import resume from "../assets/jim_walter_resume.pdf";
 import download_icon from "../assets/icons/download.png";
+
 function Experience() {
   return (
     <div className="experience section">
       <h2 className="headline">Experience</h2>
       <div className="experience-body">
         <div className="experience-leader">
-          <p>
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Minima perferendis placeat possimus dignissimos,
-            facere illo eos, excepturi distinctio aperiam molestiae amet quos vel voluptatibus.
-          </p>
+          {data.body.map((text) => (
+            <p key={text.key}>{text.copy}</p>
+          ))}
         </div>
         <a href={resume} className="download" download="jim_walter_resume">
           <p className="subexp">Click to download my resume</p>
