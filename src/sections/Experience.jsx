35f2a0999ff9a64f052bc1b4985ea92f96aa49aa
jim walter <jimwalter13090@gmail.com>
2020-08-23 13:54:36 -0600

    added UX to Experience section

--- src/sections/Experience.jsx
+++ src/sections/Experience.jsx
@@ -1,6 +1,11 @@
 import React from "react";
 import "./Experience.css";
 import data from "../json_experience.json";
+import arrow from "../assets/images/arrow.png";
+import unl from "../assets/images/unl_eng.jpg";
+import carbon from "../assets/images/carbon.jpg";
+import plc from "../assets/images/plc.jpg";
+import galv from "../assets/images/galvanize.png";
 
 import resume from "../assets/jim_walter_resume.pdf";
 import download_icon from "../assets/icons/download.png";
@@ -11,9 +16,18 @@ function Experience() {
       <h2 className="headline">Experience</h2>
       <div className="experience-body">
         <div className="experience-leader">
-          {data.body.map((text) => (
-            <p key={text.key}>{text.copy}</p>
+          {data.body.map((info) => (
+            <p key={info.key}>{info.copy}</p>
           ))}
+        </div>
+        <div className="experience-img">
+          <img src={unl} alt="University of Nebraska Engineering Logo" />
+          <img className="arrow" src={arrow} alt="University of Nebraska Engineering Logo" />
+          <img src={carbon} alt="woven carbon fiber pattern" />
+          <img className="arrow" src={arrow} alt="University of Nebraska Engineering Logo" />
+          <img src={plc} alt="" />
+          <img className="arrow" src={arrow} alt="University of Nebraska Engineering Logo" />
+          <img src={galv} alt="" />
         </div>
         <a href={resume} className="download" download="jim_walter_resume">
           <p className="subexp">Click to download my resume</p>
