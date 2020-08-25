62e2abb1d6b642a65f7bf3b7c3f2c43abe03a1c2
jim walter <jimwalter13090@gmail.com>
2020-08-25 12:46:24 -0600

    added skill section to Experience

--- src/sections/Experience.jsx
+++ src/sections/Experience.jsx
@@ -1,14 +1,29 @@
 import React from "react";
 import "./Experience.css";
+
 import data from "../json_experience.json";
+import resume from "../assets/jim_walter_resume.pdf";
+
 import arrow from "../assets/images/arrow.png";
 import unl from "../assets/images/unl_eng.jpg";
 import plc from "../assets/images/plc.jpg";
 import galv from "../assets/images/galvanize.png";
 import giide from "../assets/images/giide.png";
-
-import resume from "../assets/jim_walter_resume.pdf";
 import download_icon from "../assets/icons/download.png";
+
+import js from "../assets/icons/skills/js.jpeg";
+import aws from "../assets/icons/skills/aws.jpeg";
+import mongodb from "../assets/icons/skills/mongodb.png";
+import mySQL from "../assets/icons/skills/mySQL.png";
+import nodejs from "../assets/icons/skills/nodejs.png";
+import postgreSQL from "../assets/icons/skills/postgreSQL.png";
+import reactrouter from "../assets/icons/skills/react-router-logo.png";
+import reactjs from "../assets/icons/skills/reactjs.jpeg";
+import redis from "../assets/icons/skills/redis.png";
+import git from "../assets/icons/skills/git.png";
+import enzyme from "../assets/icons/skills/enzyme.png";
+import jest from "../assets/icons/skills/jest.jpeg";
+import htmlcss from "../assets/icons/skills/htmlcss.jpeg";
 
 function Experience() {
   return (
@@ -33,6 +48,61 @@ function Experience() {
           <p className="subexp">Click to download my resume</p>
           <img src={download_icon} alt="download from cloud icon" />
         </a>
+      </div>
+      <h2 className="tools">I have experience using...</h2>
+      <div className="exp-grid">
+        <div className="exp-item">
+          <img src={js} alt="" />
+          <p>JavaScript</p>
+        </div>
+        <div className="exp-item">
+          <img src={reactjs} alt="" />
+          <p>React.js</p>
+        </div>
+        <div className="exp-item">
+          <img src={aws} alt="" />
+          <p>AWS</p>
+        </div>
+        <div className="exp-item">
+          <img src={git} alt="" />
+          <p>Git</p>
+        </div>
+        <div className="exp-item">
+          <img src={nodejs} alt="" />
+          <p>Node.js</p>
+        </div>
+        <div className="exp-item">
+          <img src={reactrouter} alt="yikes" />
+          <p>React Router</p>
+        </div>
+        <div className="exp-item">
+          <img src={mySQL} alt="" />
+          <p>MySQL</p>
+        </div>
+        <div className="exp-item">
+          <img src={postgreSQL} alt="" />
+          <p>PostgreSQL</p>
+        </div>
+        <div className="exp-item">
+          <img src={mongodb} alt="" />
+          <p>MongoDB</p>
+        </div>
+        <div className="exp-item">
+          <img src={redis} alt="" />
+          <p>Redis</p>
+        </div>
+        <div className="exp-item">
+          <img src={enzyme} alt="" />
+          <p>Enzyme</p>
+        </div>
+        <div className="exp-item">
+          <img src={jest} alt="" />
+          <p>Jest</p>
+        </div>
+        <div className="exp-item">
+          <img src={htmlcss} alt="" />
+          <p>HTML &amp; CSS</p>
+        </div>
       </div>
     </div>
   );
